1b76100739f38a654b84baeef3cf799ae98456e5
Fabrício <fabricio_masiero@hotmail.com>
2024-06-01 12:35:53 -0300

    logics done

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+	
+	
+	var stories = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		updateUI()
     }
-
-
+	
+	@IBAction func choiceButtonPressed(_ sender: UIButton) {
+		if sender.titleLabel?.text == stories.stories[stories.currentFlow].choice1 {
+			stories.currentFlow = stories.stories[stories.currentFlow].choice1Destination
+		} else if sender.titleLabel?.text == stories.stories[stories.currentFlow].choice2 {
+			stories.currentFlow = stories.stories[stories.currentFlow].choice2Destination
+		} else if sender.titleLabel?.text == "The" || sender.titleLabel?.text == "End" {
+			print("fim")
+		}
+		
+		updateUI()
+	}
+	
+	func updateUI() {
+		storyLabel.text = stories.getLabel(stories.currentFlow)
+		choice1Button.setTitle(stories.getOptions(stories.currentFlow)[0], for: .normal)
+		choice2Button.setTitle(stories.getOptions(stories.currentFlow)[1], for: .normal)
+	}
+	
 }
 
